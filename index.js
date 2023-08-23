9d1b221aa133c4b9e719485a1d714acd8f1e75cf
Darren Glynn <darren.glynn@relexsolutions.com>
2023-08-23 16:08:49 +0300

    Review with Charlie

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Cat data
-var cats = [
+const cats = [
   { name: "Cute Cat 1", image: "cute-cat1.jpg", clicks: 0 },
   { name: "Cute Cat 2", image: "cute-cat2.jpg", clicks: 0 },
   { name: "Cute Cat 3", image: "cute-cat3.jpg", clicks: 0 },
@@ -7,7 +7,7 @@ var cats = [
   { name: "Cute Cat 5", image: "cute-cat5.jpg", clicks: 0 },
 ];
 
-let selectedCatIndex = null; // Track the index of the currently selected cat
+let catName = document.querySelector(".cat-name-header"); // Get the cat name element
 let catImage = document.querySelector(".cat-image"); // Get the cat image element
 let catClicks = document.querySelector(".clicks"); // Get the cat clicks element
 let catImageClickHandler = null; // Track the click event handler
@@ -18,39 +18,29 @@ function initializeCatList() {
 
   catListItems.forEach(function (item, index) {
     item.addEventListener("click", function () {
-      selectedCatIndex = index; // Update the selected cat index
-      updateCatDisplay();
+      updateCatDisplay(cats[index]);
     });
   });
+  updateCatDisplay(cats[0]);
 }
 
 // Function to update cat display
-function updateCatDisplay() {
-  let catName = document.querySelector(".cat-name-header");
+function updateCatDisplay(cat) {
+  catName.textContent = cat.name;
+  catImage.src = cat.image;
+  catClicks.textContent = "clicks " + cat.clicks;
 
-  if (selectedCatIndex !== null) {
-    let cat = cats[selectedCatIndex];
-
-    catName.textContent = cat.name;
-    catImage.src = cat.image;
-    catClicks.textContent = "clicks " + cat.clicks;
-
-    // Remove click event listener from the previous cat image
-    if (catImageClickHandler) {
-      catImage.removeEventListener("click", catImageClickHandler);
-    }
-
-    // Add click event listener to the cat image
-    catImageClickHandler = function () {
-      cat.clicks++;
-      catClicks.textContent = "clicks " + cat.clicks;
-    };
-    catImage.addEventListener("click", catImageClickHandler);
-  } else {
-    catName.textContent = "";
-    catImage.src = "";
-    catClicks.textContent = "";
+  // Remove click event listener from the previous cat image
+  if (catImageClickHandler) {
+    catImage.removeEventListener("click", catImageClickHandler);
   }
+
+  // Add click event listener to the cat image
+  catImageClickHandler = function () {
+    cat.clicks++;
+    catClicks.textContent = "clicks " + cat.clicks;
+  };
+  catImage.addEventListener("click", catImageClickHandler);
 }
 
 // Call the initializeCatList function to set up the initial state
